0387bb6fcce34986f92c20c4468048f44adea5a0
FHAO-3 <62599735+FHAO-3@users.noreply.github.com>
2025-08-10 03:20:35 -0300

    update | deletando 'db.sqlite3', atualizando o footer, arruamando '.gitignore', add  url de Marcas, add botao para pagina congigurações, add url para criar Marcas, iniciando views de criar Marcas, add entrada de texto dentro de 'brand_list.html' para poder fazer pesquisa por nome

--- brands/views.py
+++ brands/views.py
@@ -1,8 +1,27 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from . import models
+from django.urls import reverse_lazy
 
 
 class BrandListView(ListView):
     model = models.Brand
     template_name = 'brand_list.html'
-    context_object_name = 'brands'  # usado ao enves de mandar o `render`
+    context_object_name = 'brands'  # usado ao inves de mandar o `render`
+
+    def get_queryset(self):
+        '''
+        Metodo sera usada para fazer requisições para pesquisar por `name` como por exemplo "url.com/?name=brand"
+        '''
+        queryset = super().get_queryset()
+        name = self.request.GET.get('name')
+
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
+
+
+class BrandCreateView(CreateView):
+    model = models.Brand
+    form_class = ...
+    template_name = 'brand_create.html'
+    success_url = reverse_lazy('brands_list')

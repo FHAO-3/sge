d5274f220c713d82f811b9f7390da1741d0b2dd7
FHAO-3 <62599735+FHAO-3@users.noreply.github.com>
2025-09-01 14:18:32 -0300

    consertando erro em permissoes do supplier

--- suppliers/views.py
+++ suppliers/views.py
@@ -12,7 +12,7 @@ class SupplierListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     # 'context_object_name' usado ao inves de mandar o `render`
     paginate_by = 10
     # 'paginate_by' não pode mostrar mais de (neste caso) 10 nomes da lista
-    permission_required = 'suppliers.view_supplie'
+    permission_required = 'suppliers.view_supplier'
 
     def get_queryset(self):
         '''
@@ -33,13 +33,13 @@ class SupplierCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView
     success_url = reverse_lazy('supplier_list')
     # reverse_lazy usado para evitar problemas de importação circular de urls e views no Django (é uma boa prática usar ele em class-based views)
     # 'supplier_list' é o nome da url que queremos redirecionar apos o form ser salvo com sucesso
-    permission_required = 'suppliers.add_supplie'
+    permission_required = 'suppliers.add_supplier'
 
 
 class SupplierDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = models.Supplier
     template_name = 'supplier_detail.html'
-    permission_required = 'suppliers.view_supplie'
+    permission_required = 'suppliers.view_supplier'
 
 
 class SupplierUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
@@ -47,11 +47,11 @@ class SupplierUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView
     form_class = forms.SupplierForm
     template_name = 'supplier_update.html'
     success_url = reverse_lazy('supplier_list')
-    permission_required = 'suppliers.change_supplie'
+    permission_required = 'suppliers.change_supplier'
 
 
 class SupplierDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     model = models.Supplier
     template_name = 'supplier_delete.html'
     success_url = reverse_lazy('supplier_list')
-    permission_required = 'suppliers.delete_supplie'
+    permission_required = 'suppliers.delete_supplier'

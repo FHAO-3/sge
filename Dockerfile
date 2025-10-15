f7b888dae5fd97713a7e09fa1ef917ac220138e8
FHAO-3 <62599735+FHAO-3@users.noreply.github.com>
2025-10-15 14:50:17 -0300

    add image Docker, add docker compose, add environment variable

--- Dockerfile
+++ Dockerfile
@@ -7,15 +7,19 @@ WORKDIR /sge
 ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
 
+RUN apt update && apt -y install cron && apt -y install nano
+
 COPY . .
 
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
 
-# RUN python manage.py migrate
+COPY ./cron /etc/cron.d/cron
+RUN chmod 0644 /etc/cron.d/cron
+RUN crontab /etc/cron.d/cron
 
 # "EXPOSE" expoem uma porta
 # EXPOSE <port>
 EXPOSE 8000
 
-CMD python manage.py migrate && python manage.py runserver 0.0.0.0:8000
+CMD cron ; python manage.py migrate && python manage.py runserver 0.0.0.0:8000
--- app/settings.py
+++ app/settings.py
@@ -10,23 +10,28 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vqw*ep!7bd-&bm29*!^b3lx2a6%c-9a$&t10&*lm0&!#@9ru60'
+SECRET_KEY = os.getenv('SECRET_KEY', 'unsafe-default-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+# quando essa contante está recebendo valor `True` é quando está em `ambiente de desenolvimento`
+# quando o valor é igual a `False` quer dizer que está em `ambinete de produção`
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
